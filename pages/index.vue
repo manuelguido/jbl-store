e9c9332c309234cc95b7af7c1ed0effe981eaf9a
manuelguido <manuelguido.gb@gmail.com>
2023-10-31 15:42:32 -0300

    add: script to index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,17 @@
 <template>
   <!-- Homepage -->
   <div class="homepage bg-white-primary">
-    <view-homepage-section-a/>
-    <view-homepage-section-b/>
+    <view-homepage-section-a />
+    <view-homepage-section-b />
   </div>
   <!-- /.Homepage -->
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({});
+</script>
 
 <style lang="scss" scoped>
 .homepage {
